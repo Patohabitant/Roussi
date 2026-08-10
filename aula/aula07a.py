e0d32efcaff702533aa9b7f939d7eaa8d45ea6a0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-10 10:59:55 -0300

    Initial commit

---
+++ aula/aula07a.py
@@ -0,0 +1,19 @@
+eng2sp = dict()
+print(eng2sp)
+
+eng2sp['one'] = "uno"
+print(eng2sp)
+
+eng2sp = {"one": "uno",
+"two": "dos",
+"three": "tres"}
+print(eng2sp["two"])
+
+print(len(eng2sp))
+
+#operador IN
+print("uno" in eng2sp)
+
+#valores
+valores_dict = eng2sp.values()
+print("uno" in valores_dict)
